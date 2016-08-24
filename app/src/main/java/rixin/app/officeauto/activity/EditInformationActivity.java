4e5d192127467b197803945a78d5ed099a7b7d43
EggUncle <cicadashadow@gmail.com>
2016-08-24 23:10:47 +0800

    加入了新的图片和编辑资料页面

---
+++ app/src/main/java/rixin/app/officeauto/activity/EditInformationActivity.java
@@ -0,0 +1,15 @@
+package rixin.app.officeauto.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rixin.app.officeauto.R;
+
+public class EditInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_information);
+    }
+}
